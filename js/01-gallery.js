f9004bedceb9ed953a1514a355ed0565e64a0c33
Onufriieva <onufrieva.julia@gmail.com>
2022-07-09 12:56:43 +0300

    Task 7.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,40 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного 
-// изображения в модальном окне. Посмотри демо видео работы галереи.
-
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-//  <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-//  </div> 
-// Реализация делегирования на div.gallery и получение url большого изображения.
-
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй
-//  CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-
-
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую
-//  разметку модального окна с изображением из примеров библиотеки basicLightbox.
-
-// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь
-//  будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
-
-// Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только
-//  пока открыто модальное окно. 
-// У библиотеки basicLightbox есть метод для программного закрытия модального окна.
-
 
 
 const gallery = document.querySelector('.gallery');
@@ -44,10 +10,6 @@ gallery.addEventListener('click', onGalleryImgClick);
 
 const galleryMarkup = createGalleryItemMarkup(galleryItems);
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-// const image = document.querySelector("img");
-// console.log(image.dataset.source);
-
 
 console.log(createGalleryItemMarkup(galleryItems))
 
@@ -96,3 +58,4 @@ function onKeyPress(event) {
     return;
 };
 };
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, 
+// которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна,
+//  а также пролистывание изображений при помощи клавиатуры. Посмотри демо видео
+//   работы галереи с подключенной библиотекой.
+
+
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+// Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt.
+//  Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+const link = document.querySelector('.gallery__item');
+console.log(link)
+
+function createGalleryItemMarkup(cards) {
+    return cards.map(({ description, original, preview }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__item" 
+    href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}"/>
+  </a>
+   </li>`
+    }).join(' ');
+} 
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery__item', { 
+    captionPosition: 'bottom',
+    // captionSelector: 'img',
+    captionsData: 'alt',
+    // animationSpeed:	250,
+    // fadeSpeed:	250,
+    // docClose: true,
+    // nav: true,
+ });
+
+ console.log(lightbox)
